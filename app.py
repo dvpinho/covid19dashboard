8369b9bca1aface21c27d5e6149ea3a6098533e8
dvpinho <davidvpinho@gmail.com>
2020-05-26 10:52:58 +0100

    modified mapbox style

--- app.py
+++ app.py
@@ -580,9 +580,7 @@ def update_map(toggle):
                                 'daily_diff': 'New cases since yesterday'
                             })
 
-    MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
-
-    fig.update_layout(mapbox_style='dark', mapbox_accesstoken=MAPBOX_ACCESS_TOKEN, coloraxis_showscale=False)
+    fig.update_layout(mapbox_style='carto-darkmatter', coloraxis_showscale=False)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return fig
